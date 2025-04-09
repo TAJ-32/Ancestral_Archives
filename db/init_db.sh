58a48e699844a9458f455c62547aef5cf244b99f
Sartaj Singh <sartajs2002@gmail.com>
2025-04-08 20:58:35 -0400

    backend still exiting with 127 but getting closer

--- db/init_db.sh
+++ db/init_db.sh
@@ -2,7 +2,7 @@
 # init.sh - Substitute the environment variables and run SQL
 
 # Substitute environment variables into the SQL file
-envsubst < /docker-entrypoint-initdb.d/init.sql > /docker-entrypoint-initdb.d/init_with_values.sql
+envsubst < /docker-entrypoint-initdb.d/init.sql > /docker-entrypoint-initdb.d/init.sql
 
 # Run the SQL file that has the substituted values
-psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/init_with_values.sql
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/init.sql
